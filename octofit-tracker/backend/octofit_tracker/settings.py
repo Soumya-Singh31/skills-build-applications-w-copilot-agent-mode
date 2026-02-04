48fcdfa3601727cac041f212e664e1f42599da22
Soumya-Singh31 <soumya.singh3@gds.ey.com>
2026-02-04 18:10:55 +0000

    Configure DATABASES to use djongo (MongoDB)

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'octofit_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        # Add other required options as appropriate: HOST, PORT, USER, PASSWORD
+        # Example:
+        # 'CLIENT': {
+        #     'host': 'mongodb://localhost:27017',
+        # }
     }
 }
 
